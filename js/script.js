21d49a614f5db5f492007d4390b2734db13e86bd
Magdalena Muzalewska <magda.muzalewska@wp.pl>
2024-02-18 17:29:32 +0100

    add active link

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 const titleClickHandler = function(event){
-    console.log('Link was clicked!')
+    const clickedElement = this;
+    console.log("Link was clicked");
     console.log(event);
+    
     /* remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
-
 const activeArticels = document.querySelectorAll('.article-active');
 for(let activeArticle of activeArticels){
     activeArticle.classList.remove('active');
 }
 
   /* add class 'active' to the clicked link */
+  console.log('clickedElement (with plus): ' + clickedElement);
+  clickedElement.classList.add('active');
+
 
   /* remove class 'active' from all articles */
 
